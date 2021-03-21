91739c313c641c5ab17da94b5a68a92f4175cfea
Joseph Tastet <Josephwtastet@gmail.com>
2021-03-21 12:58:18 -0400

    changed saved beverages

--- db/migrate/20210321161101_create_saved_beverages.rb
+++ db/migrate/20210321161101_create_saved_beverages.rb
@@ -1,8 +1,8 @@
 class CreateSavedBeverages < ActiveRecord::Migration[6.0]
   def change
     create_table :saved_beverages do |t|
-      t.int :user_id
-      t.int :beverage_id
+      t.integer :user_id
+      t.integer :beverage_id
 
       t.timestamps
     end
