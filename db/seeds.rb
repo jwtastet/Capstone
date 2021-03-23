7c273d07617e417fbad35296f83e923e868248fd
Joseph Tastet <Josephwtastet@gmail.com>
2021-03-22 21:00:16 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,6 +60,7 @@ Prescription.create!({ mood_id: 3, beverage_type_id: 2, taste1_id: 1, taste2_id:
 Prescription.create!({ mood_id: 3, beverage_type_id: 2, taste1_id: 3, taste2_id: 2, flavor_id: 9, beverage_id: 7 })
 Prescription.create!({ mood_id: 3, beverage_type_id: 2, taste1_id: 1, taste2_id: 4, flavor_id: 4, beverage_id: 8 })
 Prescription.create!({ mood_id: 1, beverage_type_id: 3, taste1_id: 6, taste2_id: 1, flavor_id: 3, beverage_id: 4 })
+User.create!({ name: "Joey", email: "joey@test.com", password: "password", password_confirmation: "password" })
 SavedBeverage.create!({ user_id: 1, beverage_id: 1 })
 SavedBeverage.create!({ user_id: 1, beverage_id: 2 })
 SavedBeverage.create!({ user_id: 1, beverage_id: 3 })
